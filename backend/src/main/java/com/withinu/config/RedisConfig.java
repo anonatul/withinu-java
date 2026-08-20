64ec16d519b7d1b0f919b4a50b8ebe6e4e0cba18
Atul <atul.kevat28@gmail.com>
2026-08-20 21:59:00 +0530

    Add Render deployment support

--- backend/src/main/java/com/withinu/config/RedisConfig.java
+++ backend/src/main/java/com/withinu/config/RedisConfig.java
@@ -1,13 +1,43 @@
 package com.withinu.config;
 
+import io.lettuce.core.RedisURI;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties properties) {
+        if (properties.getUrl() != null && !properties.getUrl().isBlank()) {
+            RedisURI uri = RedisURI.create(properties.getUrl());
+            RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+            config.setHostName(uri.getHost());
+            config.setPort(uri.getPort());
+            if (uri.getUsername() != null) {
+                config.setUsername(uri.getUsername());
+            }
+            if (uri.getPassword() != null) {
+                config.setPassword(RedisPassword.of(new String(uri.getPassword())));
+            }
+            LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
+                LettuceClientConfiguration.builder();
+            if (uri.isSsl()) {
+                builder.useSsl();
+            }
+            return new LettuceConnectionFactory(config, builder.build());
+        }
+        return new LettuceConnectionFactory(
+            new RedisStandaloneConfiguration(properties.getHost(), properties.getPort()));
+    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
